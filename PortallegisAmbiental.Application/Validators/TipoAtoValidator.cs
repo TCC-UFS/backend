fe3e39ad16dd3565f3a79a140c40d0a67f7ca5b5
AllaphyUFS <allaphy@academico.ufs.br>
2023-11-11 11:43:04 -0300

    feat: adding tipo ato validations.

---
+++ PortallegisAmbiental.Application/Validators/TipoAtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using PortalLegisAmbiental.Domain.Dtos.Requests;
+
+namespace PortalLegisAmbiental.Application.Validators
+{
+    public class AddTipoAtoValidator : AbstractValidator<AddTipoAtoRequest>
+    {
+        public AddTipoAtoValidator()
+        {
+            RuleFor(tipoAto => tipoAto.Nome)
+                .NotEmpty()
+                .MinimumLength(8)
+                .MaximumLength(32);
+        }
+    }
+
+    public class UpdateTipoAtoValidator : AbstractValidator<TipoAtoRequest>
+    {
+        public UpdateTipoAtoValidator()
+        {
+            RuleFor(tipoAto => tipoAto.Nome)
+                .MinimumLength(8)
+                .MaximumLength(32);
+
+            RuleFor(tipoAto => tipoAto.Id)
+                .NotEmpty();
+        }
+    }
+}
---
+++ PortallegisAmbiental.Application/Validators/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PortalLegisAmbiental.Application.Validators
+{
+    internal static class Utils
+    {
+        internal static bool IsEmail(string email)
+        {
+            var emailRx = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+            if (!emailRx.IsMatch(email))
+                return false;
+
+            return true;
+        }
+    }
+}
