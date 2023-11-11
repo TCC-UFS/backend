d911ad68cfe5468432ec7fa661521d3592ab21fd
AllaphyUFS <allaphy@academico.ufs.br>
2023-11-11 11:42:52 -0300

    feat: adding mappers.

---
+++ PortallegisAmbiental.Application/Mappers/Mapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PortalLegisAmbiental.Domain.Dtos.Requests;
+using PortalLegisAmbiental.Domain.Dtos.Responses;
+using PortalLegisAmbiental.Domain.Entities;
+
+namespace PortalLegisAmbiental.Application.Mappers
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<AddTipoAtoRequest, TipoAto>();
+            CreateMap<TipoAto, TipoAtoResponse>();
+        }
+    }
+}
