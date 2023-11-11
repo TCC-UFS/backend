353dfd479c9108c9cbea0b4e93f9b88003b67754
AllaphyUFS <allaphy@academico.ufs.br>
2023-11-11 11:43:15 -0300

    feat: adding tipo ato services.

---
+++ PortallegisAmbiental.Application/Services/Interfaces/ITipoAtoService.cs
@@ -0,0 +1,14 @@
+﻿using PortalLegisAmbiental.Domain.Dtos.Requests;
+using PortalLegisAmbiental.Domain.Dtos.Responses;
+
+namespace PortalLegisAmbiental.Application.Services.Interfaces
+{
+    public interface ITipoAtoService
+    {
+        Task Add(AddTipoAtoRequest tipoAtoRequest);
+        Task<List<TipoAtoResponse>> SearchByName(string? name);
+        Task<TipoAtoResponse> GetById(ulong id);
+        Task Update(TipoAtoRequest tipoAtoRequest);
+        Task Disable(ulong id);
+    }
+}
---
+++ PortallegisAmbiental.Application/Services/TipoAtoService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using FluentValidation;
+using PortalLegisAmbiental.Application.Services.Interfaces;
+using PortalLegisAmbiental.Domain.Dtos.Requests;
+using PortalLegisAmbiental.Domain.Dtos.Responses;
+using PortalLegisAmbiental.Domain.Entities;
+using PortalLegisAmbiental.Domain.Exceptions;
+using PortalLegisAmbiental.Domain.IRepositories;
+
+namespace PortalLegisAmbiental.Application.Services
+{
+    public class TipoAtoService : ITipoAtoService
+    {
+        private readonly ITipoAtoRepository _tipoAtoRepository;
+        private readonly IValidator<AddTipoAtoRequest> _addValidator;
+        private readonly IValidator<TipoAtoRequest> _updateValidator;
+        private readonly IMapper _mapper;
+
+        public TipoAtoService(ITipoAtoRepository tipoAtoRepository, IMapper mapper, 
+            IValidator<AddTipoAtoRequest> addValidator, IValidator<TipoAtoRequest> updateValidator)
+        {
+            _tipoAtoRepository = tipoAtoRepository;
+            _mapper = mapper;
+            _addValidator = addValidator;
+            _updateValidator = updateValidator;
+        }
+
+        public async Task Add(AddTipoAtoRequest tipoAtoRequest)
+        {
+            _addValidator.ValidateAndThrow(tipoAtoRequest);
+
+            var tipoAto = _mapper.Map<TipoAto>(tipoAtoRequest);
+            await _tipoAtoRepository.Add(tipoAto);
+            _tipoAtoRepository.UnitOfWork.SaveChanges();
+        }
+
+        public async Task<List<TipoAtoResponse>> SearchByName(string? name)
+        {
+            if (name == null) name = string.Empty;
+            var tiposAtos = await _tipoAtoRepository.SearchByName(name, true);
+            return _mapper.Map<List<TipoAtoResponse>>(tiposAtos);
+        }
+
+        public async Task<TipoAtoResponse> GetById(ulong id)
+        {
+            var tipoAto = await _tipoAtoRepository.GetById(id, true);
+            
+            if (tipoAto == null)
+                throw new PortalLegisDomainException("KEY_NOT_FOUND", "Id não encontrado.", System.Net.HttpStatusCode.NotFound);
+
+            return _mapper.Map<TipoAtoResponse>(tipoAto);
+        }
+
+        public async Task Update(TipoAtoRequest tipoAtoRequest)
+        {
+            _updateValidator.ValidateAndThrow(tipoAtoRequest);
+
+            var tipoAto = await _tipoAtoRepository.GetById(tipoAtoRequest.Id);
+
+            if (tipoAto == null)
+                throw new PortalLegisDomainException("KEY_NOT_FOUND", "Id não encontrado.", System.Net.HttpStatusCode.NotFound);
+
+            tipoAto.UpdateName(tipoAtoRequest.Nome);
+            _tipoAtoRepository.UnitOfWork.SaveChanges();
+        }
+
+        public async Task Disable(ulong id)
+        {
+            var tipoAto = await _tipoAtoRepository.GetById(id);
+
+            if (tipoAto == null)
+                throw new PortalLegisDomainException("KEY_NOT_FOUND", "Id não encontrado.", System.Net.HttpStatusCode.NotFound);
+
+            tipoAto.Disable();
+            _tipoAtoRepository.UnitOfWork.SaveChanges();
+        }
+    }
+}
