bf8fdf009ecc8e6d017714d6c504d7ae2bc12169
AllaphyUFS <allaphy@academico.ufs.br>
2023-11-15 23:50:26 -0300

    teste

--- PortalLegisAmbiental.API/Controllers/HealthcheckController.cs
+++ PortalLegisAmbiental.API/Controllers/HealthcheckController.cs
@@ -13,7 +13,7 @@ namespace PortalLegisAmbiental.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("I'm alive and runing 4. :)");
+            return Ok("I'm alive and runing 1. :)");
         }
     }
 }
